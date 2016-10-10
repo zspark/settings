c99a9100cfe4f72f6430f620635b27c0bfe55af4
Jerry Chaos <z_spark@163.com>
2016-10-10 23:11:03 +0800

    1. add dic for optix; 2. add dic for cuda;

--- vim/vim_symbol_link.bat
+++ vim/vim_symbol_link.bat
@@ -1,6 +1,7 @@
-set vimpath=C:\Program Files (x86)\Vim\
+set vimpath=D:\Program Files (x86)\Vim\
 set rcfile=_vimrc
 set vimfiles=vimfiles
+set dic=dic
 
 set file1=vim74\gvimfullscreen.dll
 set file2=vim74\vimtweak.dll
@@ -8,6 +9,7 @@ set file2=vim74\vimtweak.dll
 @echo exe...
 mklink "%vimpath%"%rcfile% %~dp0%rcfile%
 mklink /D "%vimpath%"%vimfiles% %~dp0%vimfiles%
+mklink /D "%vimpath%"%dic% %~dp0%dic%
 mklink "%vimpath%"%file1% %~dp0%file1%
 mklink "%vimpath%"%file2% %~dp0%file2%
-@echo end
+@echo
