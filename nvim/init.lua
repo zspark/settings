77d410a3b577ca8652988f123c168bec0d266d37
赵伟(10027299) <zhaow-m@glodon.com>
2022-09-14 16:01:58 +0800

    WIP

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,7 +54,7 @@ opt.updatetime=100
 --
 cmd "set syn=on"
 cmd "set syntax=enable"
---
+cmd "set listchars=tab:>-"
 --
 --
 --
@@ -90,6 +90,7 @@ _packer.startup(function(use)
   use { "zSpark/telescope.nvim", requires = { {"zSpark/plenary.nvim"} }, }
   use "zSpark/nightfox.nvim"
   use "zSpark/mason.nvim"
+  --use { 'zSpark/barbar.nvim', requires = {'zSpark/nvim-web-devicons'} }
   use { 'zSpark/lualine.nvim',
     requires = { 'zSpark/nvim-web-devicons', opt = true },
     config = function()
@@ -125,6 +126,8 @@ end)
 --
 -- /////////////////////////////////////////////////////////////////////////////////
 -- /////////// configs
+--require("barbar").setup()
+
 local cmp=require("cmp");
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
@@ -450,12 +453,127 @@ cmd [[vmap <A-j> :m'>+<cr>`<my`>mzgv`yo`z]]
 cmd [[vmap <A-k> :m'<-2<cr>`>my`<mzgv`yo`z]]
 cmd [[map <C-t><C-t> :tabnew<CR>]]
 cmd [[map <C-t><C-w> :tabclose<CR>]]
+cmd [[noremap <silent><leader>1 :tabn 1<cr>]]
+cmd [[noremap <silent><leader>2 :tabn 2<cr>]]
+cmd [[noremap <silent><leader>3 :tabn 3<cr>]]
+cmd [[noremap <silent><leader>4 :tabn 4<cr>]]
+cmd [[noremap <silent><leader>5 :tabn 5<cr>]]
+cmd [[noremap <silent><leader>6 :tabn 6<cr>]]
+cmd [[noremap <silent><leader>7 :tabn 7<cr>]]
+cmd [[noremap <silent><leader>8 :tabn 8<cr>]]
+cmd [[noremap <silent><leader>9 :tabn 9<cr>]]
+cmd [[noremap <silent><leader>0 :tabn 10<cr>]]
 --cmd [[nnoremap <CR> o<esc>]]
 --cmd [[nnoremap ? :!]]
 cmd [[inoremap jk <esc>]]
 
 
 --
+cmd [[
+function! s:GetCurrBufNames(tabCount)
+    let bufNames = {}
+    for i in range(a:tabCount)
+        let tabNum = i + 1
+        let winNum = tabpagewinnr(tabNum)
+        let buflist = tabpagebuflist(tabNum)
+        let bufNum = buflist[winNum - 1]
+        let bufName = bufname(bufNum)
+        if bufName !=# ''
+            let bufName = fnamemodify(bufName, ':~:.')
+        endif
+        let baseName = fnamemodify(bufName, ':t')
+        let bufNames[tabNum] = {}
+        let bufNames[tabNum]['fn'] = bufName
+        let bufNames[tabNum]['bn'] = baseName
+        let bufNames[tabNum]['sn'] = baseName
+    endfor
+
+    let bnGroup = {}
+    for [tabNum, name] in items(bufNames)
+        let bn = name['bn']
+        if !has_key(bnGroup, bn)
+            let bnGroup[bn] = []
+        endif
+        let bnGroup[bn] = add(bnGroup[bn], tabNum)
+    endfor
+
+    for tabNums in values(bnGroup)
+        if len(tabNums) > 1
+            for tabNum in tabNums
+                let bufNames[tabNum]['sn'] = bufNames[tabNum]['fn']
+            endfor
+        endif
+    endfor
+
+    return bufNames
+endfunction
+
+function! MyTabline()
+    let s = ''
+    let tabCount = tabpagenr('$')
+    let bufNames = s:GetCurrBufNames(tabCount)
+    for i in range(tabCount)
+        let tabNum = i + 1
+        let winNum = tabpagewinnr(tabNum)
+        let buflist = tabpagebuflist(tabNum)
+        let bufNum = buflist[winNum - 1]
+        let bufName = bufNames[tabNum]['sn']
+
+        let bufmodified = 0
+        for b in buflist
+            if getbufvar(b, '&modified')
+                let bufmodified = 1
+                break
+            endif
+        endfor
+
+        let fname = '' 
+        let buftype = getbufvar(bufNum, '&buftype')
+        if buftype ==# ''
+            let fname = bufName !=# '' ? bufName : '[No Name]'
+        elseif buftype ==# 'quickfix'
+            let fname = '[Quickfix List]'
+        elseif buftype ==# 'help'
+            let fname = '[Help]'
+        else
+            let fname = '[' . bufName . ']'
+        endif
+
+        " select the highlighting
+        let hl = tabNum == tabpagenr() ? '%#TabLineSel#' : '%#TabLine#'
+        let s .= hl
+
+        " set the tab page number (for mouse clicks)
+        let s .= '%' . tabNum . 'T'
+
+        let s .= ' [' . tabNum . '] '
+
+        if exists('g:tabline_show_wins_count')
+            let winCount = tabpagewinnr(tabNum, '$')
+            if winCount > 1
+                let s .= '%#TabWinsCount#' . winCount . hl . ' '
+            endif
+        endif
+
+        let s .= fname . ' '
+
+        if bufmodified
+            let s .= '+ '
+        endif
+    endfor
+
+    " after the last tab fill with TabLineFill and reset tab page nr
+    let s .= '%#TabLineFill#%T'
+
+    " right-align the label to close the current tab page
+    if tabCount > 1
+        let s .= '%=%#TabLine#%999XX'
+    endif
+
+    return s
+endfunction
+set tabline=%!MyTabline()
+]]
 --
 --
 --
