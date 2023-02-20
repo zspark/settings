ea3cc2f908e52ecf13052977050a1b92044ce673
赵伟(10027299) <zhaow-m@glodon.com>
2023-02-20 08:45:38 +0800

    added todo config

--- nvim/init.lua
+++ nvim/init.lua
@@ -150,9 +150,68 @@ _packer.startup(function(use)
     requires = "nvim-lua/plenary.nvim",
     config = function()
         require("todo-comments").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+            signs = true, -- show icons in the signs column
+            sign_priority = 8, -- sign priority
+            -- keywords recognized as todo comments
+            keywords = {
+                FIX = {
+                icon = " ", -- icon used for the sign, and in search results
+                color = "error", -- can be a hex color, or a named color (see below)
+                alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+                -- signs = false, -- configure signs for some keywords individually
+                },
+                TODO = { icon = " ", color = "info" },
+                HACK = { icon = " ", color = "warning" },
+                WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+                PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+                NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+                TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+            },
+            gui_style = {
+                fg = "NONE", -- The gui style to use for the fg highlight group.
+                bg = "BOLD", -- The gui style to use for the bg highlight group.
+            },
+            merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+            -- highlighting of the line containing the todo comment
+            -- * before: highlights before the keyword (typically comment characters)
+            -- * keyword: highlights of the keyword
+            -- * after: highlights after the keyword (todo text)
+            highlight = {
+                multiline = true, -- enable multine todo comments
+                multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+                multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+                before = "", -- "fg" or "bg" or empty
+                keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+                after = "fg", -- "fg" or "bg" or empty
+                pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
+                comments_only = true, -- uses treesitter to match keywords in comments only
+                max_line_len = 400, -- ignore lines longer than this
+                exclude = {}, -- list of file types to exclude highlighting
+            },
+            -- list of named colors where we try to extract the guifg from the
+            -- list of highlight groups or use the hex color if hl not found as a fallback
+            colors = {
+                error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+                warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+                info = { "DiagnosticInfo", "#2563EB" },
+                hint = { "DiagnosticHint", "#10B981" },
+                default = { "Identifier", "#7C3AED" },
+                test = { "Identifier", "#FF00FF" }
+            },
+            search = {
+                command = "rg",
+                args = {
+                "--color=never",
+                "--no-heading",
+                "--with-filename",
+                "--line-number",
+                "--column",
+                },
+                -- regex that will be used to match keywords.
+                -- don't replace the (KEYWORDS) placeholder
+                pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+                -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+            },
         }
     end
   }
@@ -351,8 +410,12 @@ local new_maker = function(filepath, bufnr, opts)
     end
   end)
 end
+--local actions = require("telescope.actions")
+--local trouble = require("trouble.providers.telescope")
 require('telescope').setup{
   defaults = {
+    mappings = {
+    },
     buffer_previewer_maker = new_maker,
     -- Default configuration for telescope goes here:
     -- config_key = value,
@@ -361,8 +424,12 @@ require('telescope').setup{
         -- map actions.which_key to <C-h> (default: <C-/>)
         -- actions.which_key shows the mappings for your picker,
         -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        ["<C-h>"] = "which_key"
-      }
+        -- ["<C-h>"] = "which_key"
+        -- ["<c-t>"] = trouble.open_with_trouble 
+      },
+      n = { 
+        -- ["<c-t>"] = trouble.open_with_trouble 
+      },
     }
   },
   pickers = {
@@ -418,6 +485,7 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
+
 -- /////////// end of config
 -- /////////////////////////////////////////////////////////////////////////////////
 --
